8093ec9dfe0594e1182dd405d78aab641be8a448
Ayşenur Yeşilbaş <aysenuryesilbas04@gmail.com>
2021-08-22 18:58:34 +0300

    Product ve Catogory entitiy'den gelen ID leri ve ID'lere gelen navigation property'leri ekledik

---
+++ ShopApp.Entities/ProductCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities
+{
+    public class ProductCategory
+    {
+        // bu class'ı Product tablosu ile Category tablosunun birleşimi olan tablo gibi düşünebiliriz
+        // iki entity'den gelen  Id lerini buraya ekledik ve
+        //consion tabloda deniyor , bunu sor?
+         public int CategoryId { get; set; }
+
+         public Category Category { get; set; }
+        // ve + her bir id'ye gelen navigation property leri buraya ekliyoruz,
+         public int ProductId { get; set; }
+
+         public Product Product { get; set; }
+    }
+}
