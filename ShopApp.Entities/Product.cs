993b6e7bce9eca6d486e87fdf9ba8677d2f0dbc0
Ayşenur Yeşilbaş <aysenuryesilbas04@gmail.com>
2021-08-24 12:36:01 +0300

    Product içine varlıklar ekledik

---
+++ ShopApp.Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public decimal Price { get; set; }
+        // fiyat alanı
+
+        public List<ProductCategory> ProductCategory { get; set; }
+
+    }
+}
