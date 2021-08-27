8ae99ecdd998cff458d128878499e247593c824c
Ayşenur Yeşilbaş <aysenuryesilbas04@gmail.com>
2021-08-27 11:40:40 +0300

    açıklama satırı ekledik

--- ShopApp.WebUI/Startup.cs
+++ ShopApp.WebUI/Startup.cs
@@ -22,7 +22,11 @@ namespace ShopApp.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages(); 
+            /* sadece buradan ,bu metotda  IProduct ile bu ikisi çaðýrýlabilir
+            // IProduct, EFCoreProductDal 
+            // IProduct, MySqlProductDal 
+            */
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
