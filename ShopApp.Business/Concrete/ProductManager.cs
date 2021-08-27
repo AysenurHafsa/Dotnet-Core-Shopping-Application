25bdeeb1cba4759c7656379722e109dd61b98290
Ayşenur Yeşilbaş <aysenuryesilbas04@gmail.com>
2021-08-27 11:38:41 +0300

    bunu silip bağımlılığımı kaldırmak için constractor ekledik ve metotlarda düzzenleme yaptık

---
+++ ShopApp.Business/Concrete/ProductManager.cs
@@ -0,0 +1,50 @@
+﻿using ShopApp.Business.Abstract;
+using ShopApp.DataAccess.Abstract;
+using ShopApp.DataAccess.Concrete.EfCore;
+using ShopApp.Entities;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Business.Concrete
+{
+    public class ProductManager : IPoductService
+    {
+        //  EfCoreProductDal _productDal = new EfCoreProductDal();   //bunu silip bağımlılığımı kaldırmak için 
+        // alttaki satırları ekledik(1,2,3)
+        private IProductDal _productDal;  //1
+
+        public ProductManager(IProductDal productDal)  //2
+        {
+            _productDal = productDal;   //3
+        }
+
+        public void Create(Product entity)
+        {
+            _productDal.Create(entity); //entity gönderince kayıt bilgisi oluşuyor
+        }
+
+        public void Delete(Product entity)
+        {
+            _productDal.Delete(entity);   // entity gönderince kayıt bilgisi siliniyor
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll().ToList(); //kayıt istiyorsak GetAll alır toliste gönderir
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.GetById(id); //tek bir kaydı istiyorsak GetById çağırıp id gönderdik 
+        }
+
+        public void Update(Product entity)
+        {
+            _productDal.Update(entity); // update işlemleri 
+        }
+    }
+}
