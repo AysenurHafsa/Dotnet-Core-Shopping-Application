9653e4ec2a500b6a1e879eeaa25ca8a165ad9927
Ayşenur Yeşilbaş <aysenuryesilbas04@gmail.com>
2021-09-09 09:26:30 +0300

    eksik CategoryManager sınıfını Business katmanına Concrete dosyasına ekledik ve implement ettik

---
+++ ShopApp.Business/Concrete/CategoryManager.cs
@@ -0,0 +1,33 @@
+﻿using ShopApp.Business.Abstract;
+using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Business.Concrete
+{
+    class CategoryManager : ICategoryService
+    {
+        public void Create(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
