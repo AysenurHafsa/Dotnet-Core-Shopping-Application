0c0ed801644b9c32b48bf92589bd2f92e5af9404
Ayşenur Yeşilbaş <aysenuryesilbas04@gmail.com>
2021-09-10 10:22:04 +0300

    business katmanının kullandığı ProductCategory ve Category bilgileri sorgusu databaseye yükleyecek olan metot DataAccess katmanında Efcore'daki class.2a eklendi.

--- ShopApp.DataAccess/Concrete/EfCore/EfCoreProductDal.cs
+++ ShopApp.DataAccess/Concrete/EfCore/EfCoreProductDal.cs
@@ -12,32 +12,41 @@ namespace ShopApp.DataAccess.Concrete.EfCore
 {           //ctrl. diyince aşağıdaki metotlerı otomatik getirdi 
     public class EfCoreProductDal : EfCoreGenericRepository<Product, ShopContext>, IProductDal
     {   //businuss katmanı bu metodu kullanıyor...
-        public IEnumerable<Product> GetPopularProducts()
-        {
-            throw new NotImplementedException();
-        }
+
 
         public Product GetProductDetails(int id)
         {
+            using (var context = new ShopContext())
+            {   //burada  ProductCategory ve Category bilgileri sorgusu databaseye yükleniyor.
+                return context.Products
+                    .Where(i => i.Id == id)
+                    .Include(i => i.ProductCategory)
+                    .ThenInclude(i => i.Category)
+                    .FirstOrDefault();
+            }
+        }
+
+        public List<Product> GetProductsByCategory(string category)
+        {
             throw new NotImplementedException();
         }
 
 
-        public List<Product> GetProductsByCategory(string category)
-        {
-            using (var context = new ShopContext())
-            {
-                var products = context.Products.AsQueryable();
+        //public List<Product> GetProductsByCategory(string category)
+        //{
+        //    using (var context = new ShopContext())
+        //    {
+        //        var products = context.Products.AsQueryable();
 
-                if (!string.IsNullOrEmpty(category)) //categoryleri listeliyor
-                {
-                    products = products
-                                .Include(i => i.ProductCategory)
-                                .ThenInclude(i => i.Category)
-                                .Where(i => i.ProductCategory.Any(a => a.Category.Name.ToLower() == category.ToLower()));
-                }
-                return products.ToList();
-            }
-        }
+        //        if (!string.IsNullOrEmpty(category)) //categoryleri listeliyor
+        //        {
+        //            products = products
+        //                        .Include(i => i.ProductCategory)
+        //                        .ThenInclude(i => i.Category)
+        //                        .Where(i => i.ProductCategory.Any(a => a.Category.Name.ToLower() == category.ToLower()));
+        //        }
+        //        return products.ToList();
+        //    }
+        //}
     }
 }
