3e05b980a25e26a76753e6a5577cb2b359db2580
Ayşenur Yeşilbaş <aysenuryesilbas04@gmail.com>
2021-09-10 10:15:22 +0300

    details sayfasına eklenecek bilgilerin bulunacağı model oluşturuldu. Ve business katmanındaki Manager'a ürün detay bilgisi için gerekli metot eklendi.

---
+++ ShopApp.WebUI/Models/ProductDetailsModel.cs
@@ -0,0 +1,17 @@
+﻿using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.Models
+{
+    public class ProductDetailsModel
+    {
+        //details sayfasına eklenecek bilgiler burada yer alır 
+
+        public Product Product { get; set; }
+
+        public List<Category> Categories { get; set; }
+    }
+}
