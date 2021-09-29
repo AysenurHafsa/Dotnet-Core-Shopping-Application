b8ffc22d97394ea78c4d3079d073577deabec966
Ayşenur Yeşilbaş <aysenuryesilbas04@gmail.com>
2021-09-29 15:19:46 +0300

    bir kategory bilgisi ve o kategorye ait olan butun  urun bilgilerini getiren using ekledik.

--- ShopApp.DataAccess/Concrete/EfCore/EfCoreCategoryDal.cs
+++ ShopApp.DataAccess/Concrete/EfCore/EfCoreCategoryDal.cs
@@ -1,4 +1,5 @@
-﻿using ShopApp.DataAccess.Abstract;
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.DataAccess.Abstract;
 using ShopApp.Entities;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,17 @@ namespace ShopApp.DataAccess.Concrete.EfCore
 {
     public class EfCoreCategoryDal : EfCoreGenericRepository<Category, ShopContext>, ICategoryDal
     {
-        
+        public Category GetByIdWithProducts(int id)
+        {
+            using (var context = new ShopContext())
+            { 
+                //bir kategory bilgisi ve o kategorye ait olan butun  urun bilgilerini getirir 
+                return context.Categories
+                    .Where(i => i.Id == id)
+                    .Include(i => i.ProductCategories)
+                    .ThenInclude(i => i.Product)
+                    .FirstOrDefault();
+            }
+        }
     }
 }
